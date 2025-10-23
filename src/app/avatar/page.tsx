fbdab121245bb06be3c8a4b407acd48cb44e1a47
alpin-win <alpinmajor@gmail.com>
2025-10-23 13:50:56 +0800

    practise the com of avatar

---
+++ src/app/avatar/page.tsx
@@ -0,0 +1,47 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+export default function AvatarDemo() {
+  return (
+    <>
+      <h1 className="py-2">Avatar Page</h1>
+      <div className="flex flex-row flex-wrap items-center gap-12">
+        {/* 默认是圆形 */}
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+
+        {/* 圆角矩形 */}
+        <Avatar className="rounded-lg">
+          <AvatarImage
+            src="https://github.com/evilrabbit.png"
+            alt="@evilrabbit"
+          />
+          <AvatarFallback>ER</AvatarFallback>
+        </Avatar>
+
+        {/* 多头像叠加的效果 */}
+        <div className="*:data-[slot=avatar]:ring-background flex -space-x-2 *:data-[slot=avatar]:ring-2 *:data-[slot=avatar]:grayscale">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <Avatar>
+            <AvatarImage
+              src="https://github.com/maxleiter.png"
+              alt="@maxleiter"
+            />
+            <AvatarFallback>LR</AvatarFallback>
+          </Avatar>
+          <Avatar>
+            <AvatarImage
+              src="https://github.com/evilrabbit.png"
+              alt="@evilrabbit"
+            />
+            <AvatarFallback>ER</AvatarFallback>
+          </Avatar>
+        </div>
+      </div>
+    </>
+  );
+}
