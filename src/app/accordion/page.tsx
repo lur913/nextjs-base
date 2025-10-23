3052b824a98f8f8c15705a5d8937c3ebd01b1bed
alpin-win <alpinmajor@gmail.com>
2025-10-23 10:43:00 +0800

    practise the com of accordion

---
+++ src/app/accordion/page.tsx
@@ -0,0 +1,51 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+export default function AccordionPage() {
+  return (
+    <div>
+      <h1>Accordion Page</h1>
+      <Accordion type="single" defaultValue="item-2" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>它支持无障碍访问吗?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>发布细节</AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-4 text-balance">
+            <p>
+              We offer worldwide shipping through trusted courier partners.
+              Standard delivery takes 3-5 business days, while express shipping
+              ensures delivery within 1-2 business days.
+            </p>
+            <p>
+              All orders are carefully packaged and fully insured. Track your
+              shipment in real-time through our dedicated tracking portal.
+            </p>
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>退款政策</AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-4 text-balance">
+            <p>
+              We stand behind our products with a comprehensive 30-day return
+              policy. If you&apos;re not completely satisfied, simply return the
+              item in its original condition.
+            </p>
+            <p>
+              Our hassle-free return process includes free return shipping and
+              full refunds processed within 48 hours of receiving the returned
+              item.
+            </p>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+}
