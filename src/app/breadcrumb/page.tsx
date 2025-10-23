1f91362fec2527d01b889998c0ee643b8fc8d8bd
alpin-win <alpinmajor@gmail.com>
2025-10-23 14:45:25 +0800

    practise the com of breadcrumb

---
+++ src/app/breadcrumb/page.tsx
@@ -0,0 +1,41 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import Link from "next/link";
+
+/**
+ * 还有一种高级用法，结合下拉菜单 和 BreadcrumbEllipsis，等后面
+ * 再来测试
+ * @returns 
+ */
+export default function Page() {
+  return (
+    <div>
+      <h1 className="py-4">breadcrumb page</h1>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/">Home</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/button">Button</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
+}
