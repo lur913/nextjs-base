638a87ed1374eb6a9f7437d821cab455fe19214e
zhangxingping <ping.zx@qq.com>
2025-10-25 19:43:22 +0800

    practise select

---
+++ src/app/select/page.tsx
@@ -0,0 +1,39 @@
+import * as React from "react"
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+export default function SelectDemo() {
+  return (
+    <Select>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Select a fruit" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Fruits</SelectLabel>
+          <SelectItem value="apple">Apple</SelectItem>
+          <SelectItem value="banana">Banana</SelectItem>
+          <SelectItem value="blueberry">Blueberry</SelectItem>
+          <SelectItem value="grapes">Grapes</SelectItem>
+          <SelectItem value="pineapple">Pineapple</SelectItem>
+        </SelectGroup>
+        <SelectGroup>
+          <SelectLabel>Animal</SelectLabel>
+          <SelectItem value="tigger">Tigger</SelectItem>
+          <SelectItem value="horse">Horse</SelectItem>
+          <SelectItem value="blueberry">Pig</SelectItem>
+          <SelectItem value="grapes">Cat</SelectItem>
+          <SelectItem value="pineapple">Dog</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  )
+}
