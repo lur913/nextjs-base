16a5dd3f5ffb7acfbc8cafa4c7116178c7eee1bd
zhangxingping <ping.zx@qq.com>
2025-10-31 12:55:34 +0800

    完成blog页面

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,10 +1,29 @@
+import { CustomMDX } from "@/components/mdx";
+import { getBlogPosts, formatDate } from "@/utils/blog";
+
+
 export default async function Blog(
   {params}:{
     params: Promise<{slug: string}>
   }
 ) {
   const { slug } = await params
+  let post: any = getBlogPosts().find((post) => post.slug === slug)
+  const baseUrl = 'http://localhost:3000'
   return (
-    <div>This is blog page - { slug }</div>
+    
+    <section>
+      <h1 className="title font-semibold text-2xl tracking-tighter">
+        {post.metadata.title}
+      </h1>
+      <div className="flex justify-between items-center mt-2 mb-8 text-sm">
+        <p className="text-sm text-neutral-600 dark:text-neutral-400">
+          {formatDate(post.metadata.publishedAt)}
+        </p>
+      </div>
+      <article className="prose">
+        <CustomMDX source={post.content} />
+      </article>
+    </section>
   );
 }
