515c1657718ec6399f8c75e74922f89668d0aecf
alpin-win <alpinmajor@gmail.com>
2025-11-06 10:41:29 +0800

    使用negotiator库检查用户浏览器中accept-language 的语言偏好返回相应的语言响应

--- README.md
+++ README.md
@@ -13,3 +13,13 @@ bun dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+## Practice Content
+使用 Negotiator 库来处理请求头。
+跟进请求头的语言，返回相应的语言响应内容；
+很有意思的用法，示例文件：
+```
+src\app\api\negotiator\route.ts
+```
+
+
---
+++ src/app/api/negotiator/route.ts
@@ -0,0 +1,16 @@
+import Negotiator from 'negotiator';
+
+
+export async function GET(req: Request) {
+  const headers = Object.fromEntries(req.headers.entries())
+  console.log(`headers: `, headers);
+  const negotiator = new Negotiator({ headers });
+
+  // 有效的语言
+  const availableLang = ['en', 'zh-CN']
+  const lang = negotiator.language(availableLang)
+  console.log(1111, lang);
+  const body = lang === 'en' ? 'Hello World' : '你好，世界'
+
+  return new Response(body)
+}
